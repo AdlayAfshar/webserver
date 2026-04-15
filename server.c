873b9e3d95c8da0de70bd8c2d04d787a7b84c46c
AdlayAfshar <adlayafshar@gmail.com>
2026-04-15 21:06:09 +0100

    GET /products/:id

--- server.c
+++ server.c
@@ -4,6 +4,29 @@
 #include <netinet/in.h>
 #include <stdlib.h>
 
+typedef struct s_cart_item
+{
+    int product_id;
+    int quantity;
+}   t_cart_item;
+
+t_cart_item cart[100];
+int cart_count = 0;
+
+typedef struct s_product
+{
+    int     id;
+    char    *name;
+}   t_product;
+
+t_product products[] = {
+    {1, "Shoes"},
+    {2, "Bag"},
+    {3, "Hat"}
+};
+
+int products_count = 3;
+
 int main()
 {
     int server_fd, client_fd;
@@ -39,14 +62,16 @@ int main()
         printf("Buffer:\n");
         printf("==================================\n");
         printf("%s\n", buffer);
-        printf("----------------------------------\n");
+        printf("+++++++++++++++++++++++++++++++++++\n");
 
         sscanf(buffer, "%s %s %s %s", method, path, harchi, harchi2);
 
+        printf("----------------------------------\n");
         printf("Method: %s\n", method);
         printf("Path: %s\n", path);
         printf("harchi: %s\n", harchi);
-        printf("harchi2: %s\n", harchi);
+        printf("harchi2: %s\n", harchi2);
+        printf("--------------\n");
 
         char *body = strstr(buffer, "\r\n\r\n");
 
@@ -55,6 +80,7 @@ int main()
             body += 4; // رد شدن از \r\n\r\n
             printf("Body: %s\n", body);
         }
+        printf("----------------------------------\n");
 
 
         if (strcmp(method, "GET") == 0 && strcmp(path, "/health") == 0)
@@ -69,42 +95,148 @@ int main()
         }
         else if (strcmp(method, "GET") == 0 && strcmp(path, "/products") == 0)
         {
-            char *response =
+            char response[5000];
+            int i;
+            int len;
+
+            len = 0;
+            len += sprintf(response + len,
                 "HTTP/1.1 200 OK\r\n"
                 "Content-Type: application/json\r\n"
                 "\r\n"
-                "[{\"id\":1,\"name\":\"Shoes\"},{\"id\":2,\"name\":\"Bag\"}]";
+                "[");
+
+            i = 0;
+            while (i < products_count)
+            {
+                len += sprintf(response + len,
+                    "{\"id\":%d,\"name\":\"%s\"}",
+                    products[i].id,
+                    products[i].name);
+                if (i < products_count - 1)
+                    len += sprintf(response + len, ",");
+                i++;
+            }
+            len += sprintf(response + len, "]");
 
             write(client_fd, response, strlen(response));
         }
+        // else if (strcmp(method, "GET") == 0 && strcmp(path, "/products") == 0)
+        // {
+        //     char *response =
+        //         "HTTP/1.1 200 OK\r\n"
+        //         "Content-Type: application/json\r\n"
+        //         "\r\n"
+        //         "[{\"id\":1,\"name\":\"Shoes\"},{\"id\":2,\"name\":\"Bag\"}]";
+
+        //     write(client_fd, response, strlen(response));
+        // }
         else if (strcmp(method, "POST") == 0 && strcmp(path, "/cart") == 0)
         {
-            int product_id = -1;
+            int product_id;
+            int i;
+            int found;
+            char response[300];
+
+            product_id = -1;
+            found = 0;
 
             if (body)
-            {
                 sscanf(body, "{\"product_id\":%d}", &product_id);
-                printf("Product ID: %d\n", product_id);
+
+            i = 0;
+            while (i < cart_count)
+            {
+                if (cart[i].product_id == product_id)
+                {
+                    cart[i].quantity++;
+                    found = 1;
+                    break;
+                }
+                i++;
             }
 
-            char response[200];
+            if (!found && product_id != -1 && cart_count < 100)
+            {
+                cart[cart_count].product_id = product_id;
+                cart[cart_count].quantity = 1;
+                cart_count++;
+            }
 
             sprintf(response,
                 "HTTP/1.1 200 OK\r\n"
                 "Content-Type: application/json\r\n"
                 "\r\n"
                 "{\"message\":\"added\",\"product_id\":%d}",
-                product_id
-            );
+                product_id);
 
             write(client_fd, response, strlen(response));
-            // char *response =
-            //     "HTTP/1.1 200 OK\r\n"
-            //     "Content-Type: application/json\r\n"
-            //     "\r\n"
-            //     "{\"message\":\"added to cart\"}";
+        }
+        else if (strcmp(method, "GET") == 0 && strcmp(path, "/cart") == 0)
+        {
+            char response[5000];
+            int i;
+            int len;
 
-            // write(client_fd, response, strlen(response));
+            len = 0;
+            len += sprintf(response + len,
+                "HTTP/1.1 200 OK\r\n"
+                "Content-Type: application/json\r\n"
+                "\r\n"
+                "[");
+
+            i = 0;
+            while (i < cart_count)
+            {
+                len += sprintf(response + len,
+                    "{\"product_id\":%d,\"quantity\":%d}",
+                    cart[i].product_id,
+                    cart[i].quantity);
+                if (i < cart_count - 1)
+                    len += sprintf(response + len, ",");
+                i++;
+            }
+
+            len += sprintf(response + len, "]");
+
+            write(client_fd, response, strlen(response));
+        }
+        else if (strcmp(method, "GET") == 0 && strncmp(path, "/products/", 10) == 0)
+        {
+            char response[500];
+            int product_id;
+            int i;
+            int found;
+
+            product_id = atoi(path + 10);
+            found = 0;
+            i = 0;
+            while (i < products_count)
+            {
+                if (products[i].id == product_id)
+                {
+                    sprintf(response,
+                        "HTTP/1.1 200 OK\r\n"
+                        "Content-Type: application/json\r\n"
+                        "\r\n"
+                        "{\"id\":%d,\"name\":\"%s\"}",
+                        products[i].id,
+                        products[i].name);
+                    write(client_fd, response, strlen(response));
+                    found = 1;
+                    break;
+                }
+                i++;
+            }
+            if (!found)
+            {
+                char *not_found =
+                    "HTTP/1.1 404 Not Found\r\n"
+                    "Content-Type: application/json\r\n"
+                    "\r\n"
+                    "{\"error\":\"product not found\"}";
+                write(client_fd, not_found, strlen(not_found));
+            }
         }
         else
         {
@@ -121,3 +253,4 @@ int main()
 
     return 0;
 }
+
